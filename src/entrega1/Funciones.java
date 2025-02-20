32fe35abbd7f8658086f2f12ef5859d0084fbdd8
Lucy <Lucy@10.100.2.120>
2025-02-20 14:05:08 +0100

    Principio de la Entrega 1

---
+++ src/entrega1/Funciones.java
@@ -0,0 +1,38 @@
+package entrega1;
+
+public class Funciones {
+	
+	//FUNCIÓN 1
+	public static Integer serie(Integer n, Integer k) {
+		assert k > 0 : "¡OJO! K debe ser mayor que 0";
+		assert n > 0 : "¡OJO! N debe ser mayor que 0";
+		assert n>k : "¡OJO! N debe ser mayor que k";
+		Integer s = 1;
+		for (int i = 0; i <= k; i++) {
+		s = s*(n - i + 1);
+		}
+		return s;
+	}
+
+	//FUNCIÓN 2
+	public static Double Funcion2(Double a1, Double r, Double k) {
+		Double s = a1;
+        for (int i = 1; i < k; i++) {
+        s = s *(a1 * Math.pow(r, i));
+        }
+        return s;
+    }
+	
+	//FUNCIÓN 3
+	public static Double numeroCombinatorio(Integer n, Integer k) {
+		assert k > 0 : "¡OJO! K debe ser mayor que 0";
+		assert n > 0 : "¡OJO! N debe ser mayor que 0";
+		assert n>=k : "¡OJO! N debe ser mayor que k";
+		Double s = 1.;
+		for (int i = 0; i < k; i++) {
+			s = s * (n - i) / (k - i);
+		}
+		return s;
+	}
+	}
+
